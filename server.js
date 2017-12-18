8c88b3dce0622b914973f6d67d14b978934b4d2d
Carmen Perezchica <crperezchica@gmail.com>
2017-12-18 15:23:43 -0800

    added database url for heroku

--- server.js
+++ server.js
@@ -27,15 +27,18 @@ app.get('/api/v1/nasa', (req, res) => {
     console.log(req.query);
     // const test = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-12-15&camera=chemcam&api_key=hJBJ2YBwo2K4VGfoZFoKtxvICroQ4cg4qMb9HpTT`;
     // superagent.get(test)
-    superagent.get(`${nasaUrl}${rover}/photos?earth_date=${date}&camera=${camera}&api_key=${MARS_API_KEY}`)
+    // earth_date=${date}&
+    superagent.get(`${nasaUrl}${rover}/photos?cearth_date=${date}&amera=${camera}&api_key=${MARS_API_KEY}`)
         .end((err, resp) => {
+            console.log(resp.body, 'not working');
             const topPix = resp.body.photos.slice(0,25).map(image => {
-                let returnImg = {
+                const returnImg = {
                     id: image.id,
                     rover: image.rover.name,
                     camera: image.camera.name,
                     url: image.img_src
-                };
+                }; 
+                console.log(image,'YaY');
                 return returnImg;
             });
             console.log(topPix);
