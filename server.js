49464510c4ac522acefcec53658be9f1b2413684
Katlyn Tucker <studioquirk@yahoo.com>
2017-12-18 18:09:23 -0800

    worked out bugs in why no images would appear, tested

--- server.js
+++ server.js
@@ -22,13 +22,16 @@ app.get('/api/v1/nasa', (req, res) => {
     const rover = req.query.rover;
     const camera = req.query.camera;
     const date = req.query.date;
-    
+
     const nasaUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/';
     console.log(req.query);
-    // const test = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-12-15&camera=chemcam&api_key=hJBJ2YBwo2K4VGfoZFoKtxvICroQ4cg4qMb9HpTT`;
-    // superagent.get(test)
-    // earth_date=${date}&
-    superagent.get(`${nasaUrl}${rover}/photos?cearth_date=${date}&amera=${camera}&api_key=${MARS_API_KEY}`)
+    //const test = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2016-04-30&api_key=hJBJ2YBwo2K4VGfoZFoKtxvICroQ4cg4qMb9HpTT`;
+    //superagent.get(test)
+    .then(function(resp) {
+        res.send(JSON.stringify(resp.body));
+    });
+
+    superagent.get(`${nasaUrl}${rover}/photos?earth_date=${date}&camera=${camera}&api_key=${MARS_API_KEY}`)
         .end((err, resp) => {
             console.log(resp.body, 'not working');
             const topPix = resp.body.photos.slice(0,25).map(image => {
@@ -37,11 +40,10 @@ app.get('/api/v1/nasa', (req, res) => {
                     rover: image.rover.name,
                     camera: image.camera.name,
                     url: image.img_src
-                }; 
-                console.log(image,'YaY');
+                };
+
                 return returnImg;
             });
-            console.log(topPix);
             res.send(topPix);
         });
 });
